74155e738375f29dd6baae5ac1526e889604bf3b
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2019-02-27 20:50:11 +0000

    Rewrite trail drrawing to use a simple static line strip

--- PKO/src/game/effects/trail.lua
+++ PKO/src/game/effects/trail.lua
@@ -1,17 +1,17 @@
 trail=graphic:subclass({
 	name="trail",
 	cs={12,13,1}, --colors
-	ld=4,									--line divisions
+	ld=16,								--line divisions
 	ds=nil,							--divisions
 	ln=32,								--length
-	md=0,								--move delta
+	md=0,									--move delta
 })
 
 function trail:init()
 	prim:init()
 	self.ds={}
 	local pos=self:getpos()
-	for i=1,self.ld do
+	for i=1,self.ld-1 do
 		add(self.ds,pos)
 	end
 end
@@ -32,53 +32,15 @@ function trail:update()
 end
 
 function trail:g_draw()
-	for i=1,#self.cs do
-		local idx = #self.cs-i+1
-		local dcs=(i-1)/#self.cs
-		local dce=i/#self.cs
-		local c=self.cs[idx]
-		for o=1,#self.ds do
-			local dds=(o-1)/#self.ds
-			local dde=o/#self.ds
-			local ds=self.ds[o]
-			local de=self.ds[o+1] or self:getpos()
-			
-			--contain
-			if(dcs < dds and dce > dde) then
-				line(
-					ds.x-0.5,
-					ds.y-0.5,
-					de.x-0.5,
-					de.y-0.5,
-					c
-				)
-			else
- 			local li=dcs >= dds and dcs <=dde
- 			local ri=dce >= dds and dce <=dde
-			
- 			if(ri and not li) then
-					local l=vec2.lerp(ds,de,self.md)
- 				line(
- 					ds.x-0.5,
- 					ds.y-0.5,
- 					l.x-0.5,
- 					l.y-0.5,
- 					c
- 				)
- 			else
-  			if(li and not ri) then
-					local l=vec2.lerp(ds,de,self.md)
-  				line(
-  					l.x-0.5,
-  					l.y-0.5,
-  					de.x-0.5,
-  					de.y-0.5,
-  					c
-  				)
-  			end
- 			end
-			end
-		end
+	local pos = self:getpos()
+
+	for i=1,self.ld-1 do
+		local p=1-(i/self.ld)
+		local c=self.cs[ceil(p*#self.cs)]
+
+		local fp=self.ds[i]
+		local tp=self.ds[i+1] or self:getpos()
+		line(fp.x,fp.y,tp.x,tp.y,c)
 	end
 	
 	graphic.g_draw(self)
