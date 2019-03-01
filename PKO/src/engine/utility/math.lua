7b9bde4560f984b8cf8fc5c8c22f80181ccef030
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2019-03-01 04:57:38 +0000

    Override atan2 in math.lua, add pi and tau constants

--- PKO/src/engine/utility/math.lua
+++ PKO/src/engine/utility/math.lua
@@ -1,6 +1,14 @@
 --math
 --utility math functions
 -------------------------------
+pi=3.14159
+tau=2*pi
+
 function lerp(a,b,d)
 	return a+d*(b-a)
+end
+
+_old_atan2 = atan2
+function atan2(v)
+	return _old_atan2(v.y,v.x)
 end
