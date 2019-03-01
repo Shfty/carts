92bff038936b20220686608a97c5717545ed569a
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2019-03-01 04:57:10 +0000

    Iterate on dbg_axis to support rotation and arbitrary base axis

--- PKO/src/engine/debug/dbg_axis.lua
+++ PKO/src/engine/debug/dbg_axis.lua
@@ -1,26 +1,27 @@
 --debug coordinate axis
 -------------------------------
 dbg_axis=prim:subclass({
-	name="debug axis"
+	name="debug axis",
+	axis=vec2:new(0,1),
+	sz=5,
+	a=0
 })
 
 function dbg_axis:draw()
 	local pos=self:getpos()
 
+	local o = (self.axis*self.sz):rotate(self.a)
+
 	line(
-		pos.x,
-		pos.y,
-		pos.x+5,
-		pos.y,
+		pos,
+		pos + o,
 		12
 	)
-	
+
 	line(
-		pos.x,
-		pos.y,
-		pos.x,
-		pos.y+5,
-		11
+		pos,
+		pos + o:perp_ccw(),
+		8
 	)
 	
 	prim.draw(self)
